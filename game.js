fedb424710c31854f8b3a62d2f226738d6bf4212
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2024-05-10 16:02:12 +0300

    Implemented functionality moving players and google, finishing game and charging points

--- game.js
+++ game.js
@@ -1,9 +1,21 @@
-class Game {
-    #settings
+export class Game {
+    #settings = {
+        gridSize: {
+            columns: 4,
+            rows: 4
+        },
+        googleJumpInterval: 2000,
+        pointsToWin: 10
+    }
     #status = 'pending'
     #player1
     #player2
     #google
+    #googleJumpIntervalId
+    #score = {
+        1: {points: 0},
+        2: {points: 0}
+    }
 
     constructor() {
     }
@@ -27,19 +39,149 @@ class Game {
         const player2Position = this.#getRandomPosition([player1Position])
         this.#player2 = new Player(2, player2Position)
 
-        const googlePosition = this.#getRandomPosition([player1Position, player2Position])
-        this.#google = new Google(googlePosition)
+        // const googlePosition = this.#getRandomPosition([player1Position, player2Position])
+        // this.#google = new Google(googlePosition)
+
+        this.#moveGoogleToRandomPosition(true)
+    }
+
+    #runGoogleJumpInterval() {
+        this.#googleJumpIntervalId = setInterval(() => {
+            this.#moveGoogleToRandomPosition()
+        }, this.#settings.googleJumpInterval)
     }
 
     async start() {
-        this.#createUnits()
+        if (this.#status === 'pending') {
+            this.#createUnits()
+            this.#status = 'in-process'
+        }
+        this.#runGoogleJumpInterval()
+    }
 
-        if (this.#status === 'pending') this.#status = 'in-process'
+    async stop() {
+        clearInterval(this.#googleJumpIntervalId)
+    }
 
+    async finishGame() {
+        clearInterval(this.#googleJumpIntervalId)
+        this.#google.position = new Position(0, 0)
+        this.#status = 'finished'
+    }
+
+    #moveGoogleToRandomPosition(excludeGoogle) {
+        let notCrossedPosition = [this.player1.position, this.player2.position]
+
+        if (!excludeGoogle) {
+            notCrossedPosition.push(this.#google.position)
+        }
+
+        this.#google = new Google(this.#getRandomPosition(notCrossedPosition))
+    }
+
+    #checkBorders(player, delta) {
+        const newPosition = player.position.clone()
+
+        if (delta.x) newPosition.x += delta.x
+        if (delta.y) newPosition.y += delta.y
+
+        if (newPosition.x < 1 || newPosition.x > this.#settings.gridSize.columns) {
+            return true
+        }
+
+        if (newPosition.y < 1 || newPosition.y > this.#settings.gridSize.rows) {
+            return true
+        }
+
+        return false
+    }
+
+    #checkOtherPlayer(movingPlayer, otherPlayer, delta) {
+        const newPosition = movingPlayer.position.clone()
+
+        if (delta.x) newPosition.x += delta.x
+        if (delta.y) newPosition.y += delta.y
+
+        return otherPlayer.position.equal(newPosition)
+    }
+
+    async #checkGoogleCatching(player) {
+
+        if (this.#google.position.equal(player.position)) {
+            this.#score[player.id].points++
+
+            if (this.#score[player.id].points === this.#settings.pointsToWin) {
+                //clearInterval(this.#googleJumpIntervalId)
+                await this.finishGame()
+            } else {
+                this.#moveGoogleToRandomPosition()
+            }
+        }
+    }
+
+    #movePlayer(movingPlayer, otherPlayer, delta) {
+        const isBorder = this.#checkBorders(movingPlayer, delta)
+        if (isBorder) return
+
+        const isOtherPlayer = this.#checkOtherPlayer(movingPlayer, otherPlayer, delta)
+        if (isOtherPlayer) return
+
+
+        if (delta.x) movingPlayer.position.x += delta.x
+        if (delta.y) movingPlayer.position.y += delta.y
+
+        this.#checkGoogleCatching(movingPlayer)
+    }
+
+    movePlayer1Right() {
+        const delta = {x: 1}
+        this.#movePlayer(this.#player1, this.#player2, delta)
+    }
+
+    movePlayer1Left() {
+        const delta = {x: -1}
+        this.#movePlayer(this.#player1, this.#player2, delta)
+    }
+
+    movePlayer1Up() {
+        const delta = {y: -1}
+        this.#movePlayer(this.#player1, this.#player2, delta)
+    }
+
+    movePlayer1Down() {
+        const delta = {y: 1}
+        this.#movePlayer(this.#player1, this.#player2, delta)
+    }
+
+    movePlayer2Right() {
+        const delta = {x: 1}
+        this.#movePlayer(this.#player2, this.#player1, delta)
+    }
+
+    movePlayer2Left() {
+        const delta = {x: -1}
+        this.#movePlayer(this.#player2, this.#player1, delta)
+    }
+
+    movePlayer2Up() {
+        const delta = {y: -1}
+        this.#movePlayer(this.#player2, this.#player1, delta)
+    }
+
+    movePlayer2Down() {
+        const delta = {y: 1}
+        this.#movePlayer(this.#player2, this.#player1, delta)
     }
 
     set settings(settings) {
-        this.#settings = settings
+        if (settings.gridSize.columns * settings.gridSize.rows < 3) {
+            throw new Error("grid size too small")
+        }
+
+        this.#settings = {...this.settings, ...settings}
+        this.#settings.gridSize = settings.gridSize ?
+            {...this.#settings.gridSize, ...settings.gridSize}
+            : this.#settings.gridSize
     }
 
     get settings() {
@@ -61,6 +203,10 @@ class Game {
     get google() {
         return this.#google
     }
+
+    get score() {
+        return this.#score
+    }
 }
 
 class Position {
@@ -68,24 +214,32 @@ class Position {
         this.x = x
         this.y = y
     }
+
+    clone() {
+        return new Position(this.x, this.y)
+    }
+
+    equal(otherPosition) {
+        return otherPosition.x === this.x && otherPosition.y === this.y
+    }
 }
 
-class Unit{
+class Unit {
     constructor(position) {
         this.position = position
     }
 }
 
-class Player extends Unit{
+class Player extends Unit {
     constructor(id, position) {
-        super (position)
+        super(position)
         this.id = id
     }
 }
 
-class Google extends Unit{
+class Google extends Unit {
     constructor(position) {
-        super (position)
+        super(position)
     }
 }
 
